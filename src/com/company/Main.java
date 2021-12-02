f76a4413dd8c159a59043de15a050b26633c6b9a
Peace27 <Peacecornell1@gmail.com>
2021-12-02 12:40:30 +0100

    MyJavaProject

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello world. This is my first project. I am liking what am doing. I am being wowed now." +
+            "Let me give it break");
+    }
+}
